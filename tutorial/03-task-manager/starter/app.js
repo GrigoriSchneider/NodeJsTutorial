2be7fe6e76fc9d2918cd94ea7f503fc48cc22b6c
GrigoriSchneider <grigorischneider@gmail.com>
2022-09-30 23:00:56 +0200

    Task manager project 50% done

---
+++ tutorial/03-task-manager/starter/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const tasks = require('./routes/tasks');
+const connectDB = require('./controllers/db/connect');
+
+// middleware
+
+app.use(express.json());
+
+// routes
+app.get('/hello', (req, res) => {
+  res.send('Task Manager App');
+});
+
+app.use('/api/v1/tasks', tasks);
+
+// app.get('/api/v1/tasks')         - get all the tasks
+// app.post('/api/v1/tasks')        - create a new task
+// app.get('/api/v1/tasks/:id')     - get single task
+// app.patch('/api/v1/taks/:id')    - update task
+// app.delete('/api/v1/tasks/:id')  - delete task
+
+const port = 3000;
+
+const start = async () => {
+  try {
+    await connectDB();
+    app.listen(port, console.log(`Server is listening on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
