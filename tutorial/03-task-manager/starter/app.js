4d9e88084bc071524831455c8bc197ff0008b3d2
GrigoriSchneider <grigorischneider@gmail.com>
2022-10-18 21:43:02 +0200

    fixed bug for user atlas can now read & write

--- tutorial/03-task-manager/starter/app.js
+++ tutorial/03-task-manager/starter/app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks');
-const connectDB = require('./controllers/db/connect');
+const connectDB = require('./db/connect');
+require('dotenv').config();
 
 // middleware
-
 app.use(express.json());
 
 // routes
@@ -17,15 +17,15 @@ app.use('/api/v1/tasks', tasks);
 // app.get('/api/v1/tasks')         - get all the tasks
 // app.post('/api/v1/tasks')        - create a new task
 // app.get('/api/v1/tasks/:id')     - get single task
-// app.patch('/api/v1/taks/:id')    - update task
+// app.patch('/api/v1/tasks/:id')   - update task
 // app.delete('/api/v1/tasks/:id')  - delete task
 
 const port = 3000;
 
 const start = async () => {
   try {
-    await connectDB();
-    app.listen(port, console.log(`Server is listening on port ${port}...`));
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`server is listening on port ${port}...`));
   } catch (error) {
     console.log(error);
   }
