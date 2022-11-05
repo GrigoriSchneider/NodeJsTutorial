188e7b2a8293ed834c5611c8b3ab04276f127e04
GrigoriSchneider <grigorischneider@gmail.com>
2022-11-05 18:25:22 +0100

    startet ErrorChecking

---
+++ tutorial/06-jobs-api/starter/controllers/jobs.js
@@ -0,0 +1,23 @@
+const getAllJobs = async (req, res) => {
+  res.send('get all jobs');
+};
+const getJob = async (req, res) => {
+  res.send('get  jobs');
+};
+const createJob = async (req, res) => {
+  res.send('creat jobs');
+};
+const updateJob = async (req, res) => {
+  res.send('update job');
+};
+const deleteJob = async (req, res) => {
+  res.send('delete job');
+};
+
+module.exports = {
+  getAllJobs,
+  getJob,
+  createJob,
+  updateJob,
+  deleteJob,
+};
