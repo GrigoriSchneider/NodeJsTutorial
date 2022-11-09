18a5e25c4aa8cd0af39de7a6df6baf0165f3d054
GrigoriSchneider <grigorischneider@gmail.com>
2022-11-09 19:09:34 +0100

    Mongoose Midleware => Generate Token - Controller => Generate Token - Instance Method => JWT_Secret and JWT_LIFETIME => Login Controller Setup => Compare Password => Auth Middleware - Setup

--- tutorial/06-jobs-api/starter/controllers/auth.js
+++ tutorial/06-jobs-api/starter/controllers/auth.js
@@ -1,18 +1,29 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
-const { BadRequestError } = require('../errors');
+const { BadRequestError, UnauthenticatedError } = require('../errors');
 
 const register = async (req, res) => {
-  // const { name, email, password } = req.body;
-  // if (!name || !email || !password) {
-  //   throw new BadRequestError('Please provide name, email and password');
-  // }
   const user = await User.create({ ...req.body });
-  res.status(StatusCodes.CREATED).json({ user });
+  const token = user.createJWT();
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 
 const login = async (req, res) => {
-  res.send(' login user');
+  const { email, password } = req.body;
+  if (!email || !password) {
+    throw new BadRequestError('Please provide email and password');
+  }
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new UnauthenticatedError('Invalid Credentials');
+  }
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError('Invalid Credentials');
+  }
+  // commpare password
+  const token = user.createJWT();
+  res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
 };
 
 module.exports = {
