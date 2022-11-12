a4cf9c5b718b6200ca366e03ceea82c2d33b3e1d
GrigoriSchneider <grigorischneider@gmail.com>
2022-11-12 12:03:37 +0100

    Remove Job

--- tutorial/06-jobs-api/starter/controllers/jobs.js
+++ tutorial/06-jobs-api/starter/controllers/jobs.js
@@ -27,10 +27,39 @@ const createJob = async (req, res) => {
   res.status(StatusCodes.CREATED).json({ job });
 };
 const updateJob = async (req, res) => {
-  res.send('update job');
+  const {
+    body: { company, position },
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+
+  if (company === '' || position === '') {
+    throw new BadRequestError('Company or Position fields cannot be empty');
+  }
+  const job = await Job.findByIdAndUpdate(
+    { _id: jobId, createdBy: userId },
+    req.body,
+    { new: true, runValidators: true }
+  );
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`);
+  }
+  res.status(StatusCodes.OK).json({ job });
 };
 const deleteJob = async (req, res) => {
-  res.send('delete job');
+  const {
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+  const job = await Job.findByIdAndRemove({
+    _id: jobId,
+    createdBy: userId,
+  });
+
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`);
+  }
+  res.status(StatusCodes.OK).send();
 };
 
 module.exports = {
