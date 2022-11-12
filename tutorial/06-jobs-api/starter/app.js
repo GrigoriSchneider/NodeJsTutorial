927fefec1d17f1c10aa71eafff0b8ac533d09f8a
GrigoriSchneider <grigorischneider@gmail.com>
2022-11-12 14:36:41 +0100

    security Setup

--- tutorial/06-jobs-api/starter/app.js
+++ tutorial/06-jobs-api/starter/app.js
@@ -1,5 +1,12 @@
 require('dotenv').config();
 require('express-async-errors');
+
+// extra security packages
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+
 const express = require('express');
 const app = express();
 
@@ -14,9 +21,18 @@ const jobsRouter = require('./routes/jobs');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
-app.use(express.json());
 // extra packages
-
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 100, // 15 minutes
+    max: 100, // limit each IP to 100 request per windowsMs
+  })
+);
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 // routes
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', authenticateUser, jobsRouter);
