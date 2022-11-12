0e2f6c5d29182f519dc88e7bb4b56f159ca2854a
GrigoriSchneider <grigorischneider@gmail.com>
2022-11-12 13:40:53 +0100

    Validation Error

--- tutorial/06-jobs-api/starter/middleware/error-handler.js
+++ tutorial/06-jobs-api/starter/middleware/error-handler.js
@@ -1,4 +1,3 @@
-const { CustomAPIError } = require('../errors');
 const { StatusCodes } = require('http-status-codes');
 const errorHandlerMiddleware = (err, req, res, next) => {
   let customError = {
@@ -7,9 +6,17 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     msg: err.message || 'Something went wrong try again later',
   };
 
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message });
+  // if (err instanceof CustomAPIError) {
+  //   return res.status(err.statusCode).json({ msg: err.message });
+  // }
+
+  if (err.name === 'ValidationError') {
+    customError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(',');
+    customError.statusCode = 400;
   }
+
   if (err.code && err.code === 11000) {
     customError.msg = `Duplicate value entered for ${Object.keys(
       err.keyValue
