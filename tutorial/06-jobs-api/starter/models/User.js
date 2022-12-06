13d6a9bd4e7dce1f6e8969b5deff39657321628d
unknown <grigorischneider@gmail.com>
2022-12-06 23:16:38 +0100

    User Schema

--- tutorial/06-jobs-api/starter/models/User.js
+++ tutorial/06-jobs-api/starter/models/User.js
@@ -1,48 +1,24 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose')
 
 const UserSchema = new mongoose.Schema({
   name: {
-    type: String,
+    tape: String,
     required: [true, 'Please provide name'],
     minlength: 3,
-    maxlength: 50,
+    maxlength: 50
   },
   email: {
-    type: String,
+    tape: String,
     required: [true, 'Please provide email'],
-    match: [
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-      'Please provide a valid email',
-    ],
-    unique: true,
+    match: [/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Please provide valid email'],
+    unique: true
   },
   password: {
-    type: String,
+    tape: String,
     required: [true, 'Please provide password'],
-    minlength: 6,
+    minlength:6,
+    maxlength: 50
   },
-});
+})
 
-UserSchema.pre('save', async function () {
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-});
-
-UserSchema.methods.createJWT = function () {
-  return jwt.sign(
-    { userId: this._id, name: this.name },
-    process.env.JWT_SECRET,
-    {
-      expiresIn: process.env.JWT_LIFETIME,
-    }
-  );
-};
-
-UserSchema.methods.comparePassword = async function (canditatePassword) {
-  const isMatch = await bcrypt.compare(canditatePassword, this.password);
-  return isMatch;
-};
-
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model('User', UserSchema)
