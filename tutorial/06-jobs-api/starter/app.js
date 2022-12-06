b0a18188aeb79efd73dc6a2a0c36e023bf0bcca4
unknown <grigorischneider@gmail.com>
2022-12-06 17:05:56 +0100

    restartet the project for training

--- tutorial/06-jobs-api/starter/app.js
+++ tutorial/06-jobs-api/starter/app.js
@@ -1,41 +1,19 @@
 require('dotenv').config();
 require('express-async-errors');
-
-// extra security packages
-const helmet = require('helmet');
-const cors = require('cors');
-const xss = require('xss-clean');
-const rateLimiter = require('express-rate-limit');
-
 const express = require('express');
 const app = express();
-
-// connectDB
-const connectDB = require('./db/connect');
-const authenticateUser = require('./middleware/authentication');
-// routers
-const authRouter = require('./routes/auth');
-const jobsRouter = require('./routes/jobs');
 
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
-// extra packages
-app.set('trust proxy', 1);
-app.use(
-  rateLimiter({
-    windowMs: 15 * 60 * 100, // 15 minutes
-    max: 100, // limit each IP to 100 request per windowsMs
-  })
-);
 app.use(express.json());
-app.use(helmet());
-app.use(cors());
-app.use(xss());
+// extra packages
+
 // routes
-app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/jobs', authenticateUser, jobsRouter);
+app.get('/', (req, res) => {
+  res.send('jobs api');
+});
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
@@ -44,7 +22,6 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URI);
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
